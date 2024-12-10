45d646c3bc6d7b475f76953c2d20dce47a9e605d
Zacklack <contact@zacklack.de>
2024-12-10 21:15:50 +0100

    Fix base path typo in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Politk-Quiz/", // Set the base path
+  base: "/Politik-Quiz/", // Set the base path
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
